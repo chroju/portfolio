b42ba5aa5d89bd42ded06ecba3a778156723faf5
chroju <chor.chroju@gmail.com>
2019-06-04 14:16:06 +0900

    WIP: render full html sample

--- main.go
+++ main.go
@@ -15,16 +15,20 @@ func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResp
 	fp := gofeed.NewParser()
 
 	feed, _ := fp.ParseURL("https://chroju.github.io/atom.xml")
-	items := feed.Items
+	ghitems := feed.Items[:3]
+	feed, _ = fp.ParseURL("https://chroju.hatenablog.jp/feed")
+	hbitems := feed.Items[:3]
 
-	tmpl := template.Must(template.New("test.html").Parse(htmlTemplate))
+	tmpl := template.Must(template.New("index.html").Parse(htmlTemplate))
 	buf := new(bytes.Buffer)
 	w := io.Writer(buf)
 
 	err := tmpl.ExecuteTemplate(w, "base", struct {
-		Title string
+		GitHubIOEntries []*gofeed.Item
+		HatenaBlogEntries []*gofeed.Item
 	}{
-		Title: items[0].Title,
+		GitHubIOEntries: ghitems,
+		HatenaBlogEntries: hbitems,
 	})
 	if err != nil {
 		log.Fatal(err)
@@ -46,10 +50,46 @@ const htmlTemplate = `
 <html lang="ja">
 <head>
 	<meta charset="UTF-8">
-	<title>test</title>
+	<title>chroju</title>
 </head>
 <body>
-    <h1>{{.Title}}</h1>
+    <h1>chroju</h1>
+    <h2>Who</h2>
+    <dl>
+        <dt>Location</dt>
+            <dd>Tokyo, Japan</dd>
+        <dt>Skills</dt>
+            <dd>AWS / Terraform / VMware / Go / Python / bash ... etc</dd>
+    </dl>
+
+    <h2>Experience</h2>
+    <dl>
+        <dt>TIS</dt>
+            <dd>System Engineer</dd>
+            <dd>2011 - 2015</dd>
+        <dt>A certain company</dt>
+            <dd>Web Operation Engineer</dd>
+            <dd>2015 - 2019</dd>
+        <dt>Freelancer</dt>
+            <dd>Site Reliability Engineer</dd>
+            <dd>2019 -</dd>
+    </dl>
+
+    <h2>Blogs (recent entries)</h2>
+    <h3><a href="https://chroju.github.io/">the world as code</a></h3>
+    <ul>
+        {{range $entry := .GitHubIOEntries }}
+        <li><a href="{{$entry.Link}}">{{$entry.Title}}</a></li>
+        {{end}}
+    </ul>
+
+    <h3><a href="https://chroju.hatenablog.jp/">the world was not enough</a></h3>
+    <ul>
+        {{range $entry := .HatenaBlogEntries }}
+        <li><a href="{{$entry.Link}}">{{$entry.Title}}</a></li>
+        {{end}}
+    </ul>
+
 </body>
 {{end}}
 `
