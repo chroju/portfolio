76f8f2b2d9e2caedd07538832728f1cca999c026
chroju <chor.chroju@gmail.com>
2019-06-04 11:39:06 +0900

    WIP: render html template

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"bytes"
+	"html/template"
+	"io"
+	"log"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/mmcdole/gofeed"
@@ -12,9 +17,22 @@ func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResp
 	feed, _ := fp.ParseURL("https://chroju.github.io/atom.xml")
 	items := feed.Items
 
+	tmpl := template.Must(template.ParseFiles("test.html"))
+	buf := new(bytes.Buffer)
+	w := io.Writer(buf)
+
+	err := tmpl.ExecuteTemplate(w, "base", struct {
+		Title string
+	}{
+		Title: items[0].Title,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	return &events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       items[0].Title,
+		Body:       string(buf.Bytes()),
 	}, nil
 }
 
